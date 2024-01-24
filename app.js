2e53c1f1d2eb65f6ad6c80018efee81fc777fa6a
Roxana Benicio <roxanayesicavictoriabenicio@gmail.com>
2024-01-24 18:06:48 -0300

    APP.js, login register

--- app.js
+++ app.js
@@ -1,56 +1,23 @@
 const express = require('express');
-const { url } = require('node:inspector');
 const app = express();
-const path = require('node:path');
+const path = require('path');
+app.use(express.static('public'));
 
 // Routes
-app.get('/', ( req, res ) =>{
-    const pathHome = path.join(__dirname, 'views/home.html')
+app.get('/', (req, res) => {
+    const pathHome = path.join(__dirname, 'public/views/home.html');
     res.sendFile(pathHome);
-})
-
-app.get('/contact', ( req, res ) =>{
-    const pathContact = path.join(__dirname, 'views/contact.html')
-    res.sendFile(pathContact);
 });
 
-app.post('/contact', ( req, res ) =>{
-
-    console.log('Se cargo la info')
-
-    res.redirect('/')
-
-})
-
-app.get('/register', ( req, res ) =>{
-    const pathContact = path.join(__dirname, 'views/contact.html')
-    res.sendFile(pathContact);
+app.get('/login', (req, res) => {
+    const pathLogin = path.join(__dirname, 'public/views/login.html');
+    res.sendFile(pathLogin);
 });
 
-app.post('/register', ( req, res ) =>{
-    console.log('Se cargo la info')
-
-    res.redirect('/login')
-
-})
-
-app.get('/about-us', ( req, res ) =>{
-    res.sendFile( path.join(__dirname, 'views/about.html') )
-})
-
-app.get('/flex', ( req, res ) =>{
-    
-
-    res.sendFile( path.join(__dirname, 'views/demo.html') )
-})
-
-
+app.get('/register', (req, res) => {
+    const pathRegister = path.join(__dirname, 'public/views/register.html');
+    res.sendFile(pathRegister);
+});
 
 // Public (static) Listen Server
-
-app.use(express.static('public'))
-
-const PORT  = process.env.PORT || 3007
-
-app.listen( PORT, () => console.log(`Server up on PORT:  http://localhost:${PORT}`) )
-
+app.listen(3007, () => console.log(`Server up on PORT:  http://localhost:3007`));
